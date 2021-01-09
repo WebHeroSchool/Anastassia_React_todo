027157d9f12df60225f427d9c74418b764a5f1f2
Anastassia Varabei <nastenkaok1984@gmail.com>
2021-01-10 00:33:26 +0300

    added file number.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {counts, length} from "./number";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+          <p>
+              {counts * length}
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
