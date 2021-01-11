a2b2aa31d211ae4eb3a9e7430fe94455cf4397f6
Anastassia Varabei <nastenkaok1984@gmail.com>
2021-01-11 22:09:23 +0300

    added list and key

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,26 @@ import InputItem from "../InputItem/InputItem";
 import ItemList from "../ItemList/ItemList";
 import Footer from "../Footer/Footer";
 
-const todoItem = 'Написать новое приложение на React';
+const App = () => {
+    const items = [
+            {
+                value: 'Написать новое приложение на React'
+            },
+            {
+                value: 'Протестировать приложение'
+            },
+            {
+                value: 'Добавить приложение в портфолио'
+            }
+    ];
 
-const App = () => (<div>
+    return (
+    <div>
     <h1 style={{color: 'red'}}>Важные дела:</h1>
     <InputItem/>
-    <ItemList todoItem={todoItem} />
+    <ItemList items={items} />
     <Footer count={3}/>
 </div>);
+};
 
 export default App;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Item from "../Item/Item";
 
-const ItemList = ({todoItem}) => (<ul>
-    <li><Item todoItem={todoItem} /></li>
-    <li><Item todoItem={'Протестировать приложение'}/></li>
-    <li><Item todoItem={'Добавить приложение в портфолио'}/></li>
+const ItemList = ({items}) => (<ul>
+    {items.map(item => <li key={item.value}>
+        <Item value={item.value} /></li>
+    )}
 </ul>);
 
 export default ItemList;
