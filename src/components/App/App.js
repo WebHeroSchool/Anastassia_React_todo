828cbe54bde46f852297517b086272f57467ef98
Anastassia Varabei <nastenkaok1984@gmail.com>
2021-01-18 22:31:29 +0300

    added class App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,9 @@ import ItemList from "../ItemList/ItemList";
 import Footer from "../Footer/Footer";
 import styles from "./App.module.css";
 
-const App = () => {
-    const items = [
+class App extends React.Component {
+    render() {
+        const items = [
             {
                 value: 'Написать новое приложение на React',
                 isDone: true
@@ -18,15 +19,16 @@ const App = () => {
                 value: 'Добавить приложение в портфолио',
                 isDone: false
             }
-    ];
+        ];
 
-    return (
-    <div className={styles.wrap}>
-    <h1 className={styles.title}>Важные дела:</h1>
-    <InputItem/>
-    <ItemList items={items} />
-    <Footer count={3}/>
-</div>);
-};
+        return (
+            <div className={styles.wrap}>
+                <h1 className={styles.title}>Важные дела:</h1>
+                <InputItem/>
+                <ItemList items={items} />
+                <Footer count={3}/>
+            </div>);
+    }
+}
 
 export default App;
